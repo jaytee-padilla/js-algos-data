ceaab11f47eaf7d8064fcba617fa253c4fd9dd3a
Jaytee <Padilla.Jaytee@gmail.com>
2020-06-17 12:15:34 -0700

    Implemented push() method for the Stack class

--- stacks.js
+++ stacks.js
@@ -7,8 +7,36 @@ class Node {
 
 class Stack {
   constructor() {
-    this.first = null;
-    this.last = null;
+    this.top = null;
+    this.bottom = null;
     this.size = 0;
   }
+
+  push(val) {
+    let newNode = new Node(val)
+
+    // if there are no nodes in the stack, set the top and bottom properties to be the newly created node
+    if(!this.top) {
+      this.top = newNode;
+      this.bottom = newNode;
+    } else {
+      // if there is at least one node, create a variable that stores the current top property on the stack
+      let temp = this.top;
+
+      // reset the top property to be the newly created node
+      this.top = newNode;
+
+      // set the next property on the node to be the previously created variable
+      this.top.next = temp;
+    }
+    
+    // increment the size of the stack by 1
+    return ++this.size;
+  }
 }
+
+let stack = new Stack();
+stack.push(1);
+stack.push(3);
+stack.push(6);
+console.log(stack);
