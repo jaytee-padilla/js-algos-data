079e13e616bcd8fbad000963473b42f1050ac107
Jaytee <Padilla.Jaytee@gmail.com>
2020-06-17 13:26:33 -0700

    Implemented dequeue() method of Queue class

--- queues.js
+++ queues.js
@@ -35,7 +35,26 @@ class Queue {
 
   // removes node from beginning of queue
   dequeue() {
+    // if there is no first property, return null
+    if(!this.first) return null;
 
+    // store first property in temporary variable
+    let temp = this.first;
+
+    // see if the first property's value is the same as the last property's value (aka, check if there is only one node)
+    // if so, set the first and last properties to be null
+    if(this.first === this.last) {
+      this.last = null;
+    }
+
+    // if there is more than 1 node, set the first property to be the value of whatever's currently stored in first.next
+    this.first = this.first.next;
+
+    // decrement the size of the queue by 1
+    this.size--;
+
+    // return the value of what was dequeued
+    return temp.value;
   }
 }
 
@@ -44,4 +63,5 @@ queue.enqueue(1);
 queue.enqueue(3);
 queue.enqueue(6);
 queue.enqueue(9);
+queue.dequeue();
 console.log(queue);
