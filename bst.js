96c32e60f1b1aec4f57dab87181fe263840c4934
Jaytee <Padilla.Jaytee@gmail.com>
2020-06-18 13:24:05 -0700

    Laid out psuedo code and basic class structure for binary search tree work

---
+++ bst.js
@@ -0,0 +1,32 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  // insert a new node into the BST
+  insert(value) {
+    // create a new node
+
+    // starting at the root,
+    // check if there is a root, if not, set the new node as the root
+    // if there is a root, check if the value of the new node is greater than or less than the value of the root
+
+    // if it is greater,
+    // check to see if there is a node to the right,
+    // if there is, move to that node and repeat these steps
+    // if there is not, add the new node as the right property
+
+    // if it is less,
+    // check ot see if there is a node to the left
+    // if there is, move to that node and repeat these steps
+    // if there is not, add the new node as the right property
+  }
+}
