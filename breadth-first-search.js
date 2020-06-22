ff476fe43048fc85f169980f118f0b6382cf2b45
Jaytee <Padilla.Jaytee@gmail.com>
2020-06-22 14:23:21 -0700

    Laid out psuedo code for breadth-first-search file

---
+++ breadth-first-search.js
@@ -0,0 +1,12 @@
+// ***** ITERATIVE *****
+// create a queue (will be using an array in this case)
+// create a variable that will store the values of nodes visited
+
+// place the root node into the queue
+
+// Loop as long as there is anything in the queue:
+// Dequeue a node from the queue and push the value of that dequeued node into the variable that stores the value of nodes visited
+// If there is a left property on the node that was dequeued, add the left property's node to the queue
+// If there is a right property on the node that was dequeued, add the right property's node to the queue
+
+// return the variable that stores the values of nodes visited
