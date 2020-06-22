bab702081a0cbcb4905390d78b820b5981e050c1
Jaytee <Padilla.Jaytee@gmail.com>
2020-06-22 15:26:50 -0700

    Minor code optimization to BFS() method

--- bst.js
+++ bst.js
@@ -104,18 +104,19 @@ class BinarySearchTree {
     // ***** ITERATIVE *****
     // create a queue (will be using an array in this case)
     // create a variable that will store the values of nodes visited
-    let queue = [];
-    let visited = [];
+    let queue = [],
+        visited = [],
+        current = this.root;
 
     // place the root node into the queue
-    queue.push(tree.root);
+    queue.push(this.root);
 
     // Loop as long as there is anything in the queue:
     // Dequeue a node from the queue and push the value of that dequeued node into the variable that stores the value of nodes visited
     // If there is a left property on the node that was dequeued, add the left property's node to the queue
     // If there is a right property on the node that was dequeued, add the right property's node to the queue
     while(queue.length > 0) {
-      let current = queue.shift();
+      current = queue.shift();
       
       visited.push(current.value);
 
