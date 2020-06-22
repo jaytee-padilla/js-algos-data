ae602297ccb13b99f8a0dee34e8fdeb7d718a403
Jaytee <Padilla.Jaytee@gmail.com>
2020-06-22 16:55:31 -0700

    Implemented DFSPostOrder() method for BinarySearchTree class

--- bst.js
+++ bst.js
@@ -162,6 +162,36 @@ class BinarySearchTree {
     // Return the 'visited array
     return visited;
   }
+
+  DFSPostOrder() {
+    // ***** RECURSIVE *****
+    // Create variable to store the values of visited nodes
+    // Store the root of the BST in a variable
+    let visited = [],
+        current = this.root;
+
+    // Write a helper function which accepts a node:
+    // If the node has a left property, call the helper function with the left property on the node
+    // If the node has a right property call the helper function with the right property on the node
+    // Push the value of the node to the 'visited' array
+    function traverse(node) {
+      if(node.left) {
+        traverse(node.left);
+      }
+
+      if(node.right) {
+        traverse(node.right);
+      }
+
+      visited.push(node.value);
+    }
+
+    // Invoke the helper function with the 'current' variable
+    traverse(current);
+
+    // Return the 'visited array
+    return visited;
+  }
 }
 
 let tree = new BinarySearchTree();
@@ -172,5 +202,6 @@ tree.insert(12);
 tree.insert(11);
 tree.insert(15);
 tree.insert(9);
-console.log(tree.BFS());
+// console.log(tree.BFS());
 console.log(tree.DFSPreOrder());
+console.log(tree.DFSPostOrder());
