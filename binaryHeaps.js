96c99c6b5850e1da3e8b7d936f93178fbda6f437
Jaytee <Padilla.Jaytee@gmail.com>
2020-06-23 15:13:49 -0700

    progress commit, about to refactor code

--- binaryHeaps.js
+++ binaryHeaps.js
@@ -5,13 +5,28 @@ class MaxBinaryHeap {
 
   insert(value) {
     // push the value parameter into this.values
+    this.values.push(value);
 
     // Bubble the value up to its correct spot:
     // Create a variable called index which is the length of the values property - 1 (grabbing the last element of the this.values array, aka the newly inserted 'value' parameter)
     // Create a variable called parentIndex whose value will be the floor of (index - 1) / 2 (grabbing the parent index of the last element in the this.values array)
+    let index = this.values.length - 1;
+    let parentIndex = Math.floor((index - 1) / 2);
     
-    // Keep looping as long as the value of the parentIndex's elements is less than the value of the child index (compare the values of the 'parentIndex' and 'index' variables checking for which value is larger)
+    // Keep looping as long as the value of the parentIndex's element is less than the value of the child index (compare the values of the 'parentIndex' and 'index' variables checking for which value is larger)
     // Swap the value of the element at the parentIndex with the value of the element at the child index (if the value of the 'index' variable is larger than the value of 'parentIndex', swap the values' positions, if not, then the bubbling is done)
     // Set the index to be the parentIndex and start over
+    while(this.values[index] > this.values[parentIndex]) {
+      let temp = this.values[parentIndex];
+
+      this.values[parentIndex] = this.values[index];
+      this.values[index] = temp;
+    }
   }
 }
+
+let heap = new MaxBinaryHeap();
+heap.insert(10);
+heap.insert(12);
+heap.insert(8);
+heap.insert(20);
