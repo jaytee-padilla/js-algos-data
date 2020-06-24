f04d4608b9e9b1836559d528667c82970c3bd442
Jaytee <Padilla.Jaytee@gmail.com>
2020-06-24 16:52:05 -0700

    Laid out psuedo code for extractMax() method for MaxBinaryHeap class

--- binaryHeaps.js
+++ binaryHeaps.js
@@ -29,6 +29,20 @@ class MaxBinaryHeap {
       index = parentIndex;
     }
   }
+
+  extractMax() {
+    // Swap the first value in the values property with the last one
+    // Pop from the values property, so you can return the value at the end
+    // Have the new root "sink down"/down heap to the correct spot:
+
+    // Parent index starts at 0 (the root)
+    // Find the index of the left child: 2 * index + 1 (make sure the child's not out of bounds)
+    // Find the index of the right child: 2 * index + 2 (make sure the child's not out of bounds)
+    // If the left or the right child is greater than the element, swap them. If both left and right children are larger, swap the largest child
+    // The child index that was swapped now become the new parent index
+    // Keep looping and swapping until neither child is larger than the element
+    // Return the old root
+  }
 }
 
 let heap = new MaxBinaryHeap();
